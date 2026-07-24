90cdc14836f0552837645e91e4c9065663399a18
Li <113808480+Li2548@users.noreply.github.com>
2026-07-25 07:52:19 +0800

    feat(ci): adding local_ci scripts

---
+++ local_ci.ps1
@@ -0,0 +1,20 @@
+﻿flutter pub get
+if (!(dart format --set-exit-if-changed .)) {
+    Write-Host "::warning title=Formatting::代码未通过 dart format；请运行 dart format ."
+}
+else {
+    Write-Host "::notice title=Formatting::代码已通过 dart format。"
+}
+
+if (!(flutter analyze)) {
+    Write-Host "::warning title=Static analysis::flutter analyze 发现异常，请查看日志。"
+}
+else {
+    Write-Host "::notice title=Static analysis::flutter analyze 未发现异常。"
+}
+
+if (Test-Path -Path "test/**/*.dart" -PathType Leaf) {
+    flutter test
+}
+python3 -m unittest discover -s pybuilds/tests -v
+Pause
